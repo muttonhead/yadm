286e6aef764ec0a5adee65f8970828f62384bd5c
Corey <1957486+muttonhead@users.noreply.github.com>
2024-06-04 14:47:31 +0000

    hello sunlight

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
-starship init fish | source
-eval (gdircolors -c ~/.config/zsh/dircolors.ansi-dark)
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
 
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
+# Created by `pipx` on 2024-05-22 20:23:23
+set PATH $PATH /home/corey/.local/bin
+set PATH $PATH /home/corey/.garden/bin
